d7cc9d66de6d7efe313adb1a9e14daaebfcd8e2b
faerie <faerie@MamaFaerie>
2014-10-09 02:14:25 +0200

    tryed some stuff

--- test.cpp
+++ test.cpp
@@ -2,52 +2,63 @@
 #include <iostream>
 #include <string>
 #include <functional>
+#include <memory>
+#include <unordered_map>
+#include <algorithm>
 
 namespace Event_bus
 {
-  class Message
-  {
-    public:
-      Message(std::string id)
-      {
-        std::hash<std::string> hash;
+	template<typename T>
+	class Message
+	{
+		public:
+		Message(std::string id, T data) : m_data(new T(data))
+		{
+			std::hash<std::string> hash;
         
-        m_id = hash(id);
-      }
-      ~Message()
-      {
-          for (auto &i : m_data)
-            delete i;
-      }
-    
-      void  Add_data(any* data)
-      {
-        m_data.push_back(data);
-      }
-      
-      void  print()
-      {
-          std::cout<<m_data[0]->Get_data();
-      }
-      
-    protected:
-      unsigned int  m_id;
-      std::vector<void*> m_data;
-  };
+			m_id = hash(id);
+		}
+
+		T*  Get_data(void) const { return m_data.get(); }
+
+		unsigned int    Get_id(void) const { return m_id; }
+
+		protected:
+			unsigned int  m_id;
+			std::auto_ptr<T> m_data;
+	};
   
-  class Bus
-  {
-    public:
-      Bus();
-      ~Bus();
-  };
+	class Bus
+	{
+		public:
+		Bus();
+		~Bus();
+
+		void    Add_listener(std::string id, std::function<void(void*)> function)
+		{
+			std::hash<std::string>  hash;
+			listeners.insert(std::pair<unsigned int, std::function<void(void*)>>(hash(id), function));
+		}
+
+		template<typename T>
+		void    Send_message(Message<T> message)
+		{
+			for (auto i : listeners.equal_range(message.Get_id()))
+			{
+				i->second(message.Get_data());
+			}
+		}
+
+		protected:
+		std::unordered_multimap<unsigned int, std::function<void(void*)>>  listeners;
+	};
 }
 
 int main()
 {
-    std::cout<<"Creating message"<<std::endl;
-    Event_bus::Message m("tada");
-    m.Add_data(new Event_bus::Data<std::string>("salut"));
-    m.print();
-    std::cout<<"Done"<<std::endl;
+	std::cout<<"Creating message"<<std::endl;
+	Event_bus::Message<std::string> m("record", std::string("hello"));
+	Event_bus::Bus  b();
+	b.Add_listener("record", [] (void *i) { std::string *tmp = static_cast<std::string*>(i); std::cout<<*tmp<<std::endl;});
+	b.Send_message(m);
 }
